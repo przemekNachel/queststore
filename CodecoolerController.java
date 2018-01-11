fc11deeae406b781c5c03b309bd70e4c3781a7a1
Irek Zagan <Sefrys@gmail.com>
2018-01-11 15:39:15 +0100

    Add method signatures

--- CodecoolerController.java
+++ CodecoolerController.java
@@ -2,4 +2,15 @@ public class CodecoolController {
   public CodecoolerView view = new CodecoolerModel();
   public CodecoolerModel currentUser = new CodecoolerModel();
 
+  public CodecoolerController(Codecooler codecooler) {
+
+  }
+
+  public void buyArtifact(String name) {
+
+  }
+
+  public void useArtifact(Artifact artifact) {
+    
+  }
 }
