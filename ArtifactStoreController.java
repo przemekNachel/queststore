7776f79330f95fca7fae1b5df0c588d3c54c8fde
creat73 <nikill1125@gmail.com>
2018-01-11 13:07:07 +0100

    Rebuild add artifact methode to use attributes instead of methodes

--- ArtifactStoreController.java
+++ ArtifactStoreController.java
@@ -4,11 +4,16 @@ import ArtifactStoreModel;
 
 class ArtifactStoreController{
     public addNewArtifact(String name, Float price, String description){
-        name = ArtifactStoreView.artifactNameQuestion();
-        price = ArtifactStoreView.artifactPriceQuestion();
-        description = ArtifactView.artifactDescriptionQuestion();
+        ArtifactStoreView view = new ArtifactStoreView();
+        String name = view.getStringFromUserInput(view.artifactNameQuestion);
+        String description = view.getStringFromUserInput(view.artifactDescriptionQuestion);
+        String priceStr = view.getStringFromUserInput(view.artifactPriceQuestion)
+        float price = Float.parseFloat(priceStr);
         ArtifactModel artifact = new ArtifactModel(name, price, description);
-        ArtifactStroreModel.addNewArtifact(artifact)
+        ArtifactStroreModel.addNewArtifact(artifact);
+        ArtiffactDaoImpl dao = new ArtiffactDaoImpl();
+        // getAllArtifacts() getIterator(),
+        assignArtifactToCategory();
     }
 
     public buyArtifact(String name, Group<User> consumers){
@@ -23,7 +28,7 @@ class ArtifactStoreController{
 
     }
 
-    public assignArtifactToCategory(){
+    public assignArtifactToCategory(ArtifactModel artifact){
 
     }
 }
--- ArtifactStoreView.java
+++ ArtifactStoreView.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
-class ArtifactStoreView{
-    public String artifactNameQuestion = "What should be the artefacts name?"
-    public String artifactPriceQuestion = "What should be its price?"
+class ArtifactStoreView extends AbstractConsoleView{
+    public String artifactNameQuestion = "What should be the artefacts name?";
+    public String artifactDescriptionQuestion = "Provide artifact description: ";
+    public String artifactPriceQuestion = "What should be its price?";
     public String artifactPurcahseQuestion;
-    public String artifactCategoryQuestion = "Which category should artifact belong to?;"
-    public String artifactUnavaliable = "This artifact is unavaliable"
-    public String insufficientFunds = "Insufficient funds"
+    public String artifactCategoryQuestion = "Which category should artifact belong to?";
+    public String artifactUnavaliable = "This artifact is unavaliable";
+    public String insufficientFunds = "Insufficient funds";
 }
