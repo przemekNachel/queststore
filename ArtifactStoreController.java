ff679b9fc59e99f81aa704c8c5b3efa48e485c0f
creat73 <nikill1125@gmail.com>
2018-01-11 14:40:39 +0100

    Add artifact group iterator

--- ArtifactStoreController.java
+++ ArtifactStoreController.java
@@ -1,34 +1,38 @@
 import ArtifactModel;
 import ArtifactStoreView;
 import ArtifactStoreModel;
+import ArtifactDaoImpl;
 
 class ArtifactStoreController{
-    public addNewArtifact(String name, Float price, String description){
+    public void addNewArtifact(String name, Float price, String description){
         ArtifactStoreView view = new ArtifactStoreView();
         String name = view.getStringFromUserInput(view.artifactNameQuestion);
         String description = view.getStringFromUserInput(view.artifactDescriptionQuestion);
         String priceStr = view.getStringFromUserInput(view.artifactPriceQuestion)
         float price = Float.parseFloat(priceStr);
         ArtifactModel artifact = new ArtifactModel(name, price, description);
-        ArtifactStroreModel.addNewArtifact(artifact);
         ArtiffactDaoImpl dao = new ArtiffactDaoImpl();
-        // getAllArtifacts() getIterator(),
+        Group<String> group = dao.getArtifactGroupNames();
+        Iterator<String> iter = group.getIterator();
+        while(iter.hasNext()){
+          System.out.println(iter.next());
+        }
         assignArtifactToCategory();
     }
 
-    public buyArtifact(String name, Group<User> consumers){
+    public void buyArtifact(String name, Group<User> consumers){
 
     }
 
-    public editArtifact(){
+    public  void editArtifact(){
 
     }
 
-    public createArtifactCategory(){
+    public void createArtifactCategory(){
 
     }
 
-    public assignArtifactToCategory(ArtifactModel artifact){
+    public void assignArtifactToCategory(ArtifactModel artifact){
 
     }
 }
