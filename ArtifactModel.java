9f43ca9a2d75a1d403fc3b9aadc28b4ba4c68fda
creat73 <nikill1125@gmail.com>
2018-01-11 10:01:12 +0100

    Create getters and setters

--- ArtifactModel.java
+++ ArtifactModel.java
@@ -3,4 +3,40 @@ public class ArtifactModel {
     private String description;
     private Float price;
     private boolean hesBeenUsed;
+
+    public String toString();{
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public boolean getUsageStatus() {
+        return hesBeenUsed;
+    }
+
+    public void setUsageStatus(boolean hesBeenUsed) {
+        this.hesBeenUsed = hesBeenUsed;
+    }
 }
