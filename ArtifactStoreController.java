78e48666a81bb7eefb84539dcb0838f48e795296
creat73 <nikill1125@gmail.com>
2018-01-11 10:24:51 +0100

    Add ArtifactStoreController blueprint

---
+++ ArtifactStoreController.java
@@ -0,0 +1,20 @@
+class ArtifactStoreController{
+    public addNewArtifact(String name, Float price){
+    }
+
+    public Artifact(String name, Group<User> consumers){
+
+    }
+
+    public editArtifact(){
+
+    }
+
+    public createArtifactCategory(){
+
+    }
+
+    public assignArtifactToCategory(){
+
+    }
+}
