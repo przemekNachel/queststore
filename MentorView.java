e8010375d55d10e3a648a55a32c386f6a1fa561c
Irek Zagan <Sefrys@gmail.com>
2018-01-11 15:08:45 +0100

    Implement mentorview class fields

--- MentorView.java
+++ MentorView.java
@@ -1,3 +1,12 @@
 public class MentorView extends AbstractConsoleView {
-  
+  public String userNicknameQuestion = "Provide user Nickname: ";
+  public String userEmailQuestion = "Provide user Email: ";
+  public String userPasswordQuestion = "Provide user Password: ";
+  public String userGroupQuestion = "Provide user Group: ";
+  public String editNicknameQuestion = "Provide new user Nickname";
+  public String editEmailQuestion = "Provide new user Email: ";
+  public String editPasswordQuestion = "Provide new user Password: ";
+  public String editGroupQuestion = "Provide new user Group: ";
+  public String markQuestNameQuestion = "Provide Quest name: ";
+  public String markArtifactUsedQuestion = "Provide name of used artifact: ";
 }
