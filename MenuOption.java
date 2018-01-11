24e55a276cc0812fd4e824bb3e44f0277849a39f
Michal <michal.hanebach@gmail.com>
2018-01-11 16:30:50 +0100

    add and implement AbstractConsoleView and necessary classes: Menu and MenuOption

---
+++ MenuOption.java
@@ -0,0 +1,26 @@
+public class MenuOption {
+
+  private String optionId;
+  private String optionName;
+
+  public MenuOption(String optionId, String optionName) {
+
+    this.optionId = optionId;
+    this.optionName = optionName;
+  }
+
+  public String getId() {
+
+    return optionId;
+  }
+
+  public String getName() {
+
+    return optionName;
+  }
+
+  public String toString() {
+    
+    return String.format("%s  %s", optionId, optionName);
+  }
+}
