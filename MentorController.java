a9142a45be05b7d98521150ac87ea920398c528a
Irek Zagan <Sefrys@gmail.com>
2018-01-11 11:33:17 +0100

    Add createCodecooler method

--- MentorController.java
+++ MentorController.java
@@ -1,3 +1,11 @@
 public class MentorController {
     public MentorView view;
+
+    public void createCodecooler() {
+      CodecoolerModel codecooler = new CodecoolerModel();
+    }
+    // Using userDaoImpl, call addUser() method with has the user passed
+    // check through day if such user already exists
+    // by dao method getUser()
+    // if returns null -- create new user, else, throw error
 }
