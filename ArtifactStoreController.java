3918f44e844d5c4140b76958e52b1de3ab357924
creat73 <nikill1125@gmail.com>
2018-01-11 15:07:43 +0100

    Add editArtifact method

--- ArtifactStoreController.java
+++ ArtifactStoreController.java
@@ -9,7 +9,7 @@ class ArtifactStoreController{
         String name = view.getStringFromUserInput(view.artifactNameQuestion);
         String description = view.getStringFromUserInput(view.artifactDescriptionQuestion);
         String priceStr = view.getStringFromUserInput(view.artifactPriceQuestion)
-        float price = Float.parseFloat(priceStr);
+        float price = float.parsefloat(priceStr);
         ArtifactModel artifact = new ArtifactModel(name, price, description);
         ArtiffactDaoImpl dao = new ArtiffactDaoImpl();
         Group<String> group = dao.getArtifactGroupNames();
@@ -21,11 +21,32 @@ class ArtifactStoreController{
     }
 
     public void buyArtifact(String name, Group<User> consumers){
+        ArtiffactDaoImpl dao = new ArtiffactDaoImpl();
+        ArtifactModel artifact = dao.getArtifact(name);
 
     }
 
-    public  void editArtifact(){
-
+    public void editArtifact(){
+        ArtiffactDaoImpl dao = new ArtiffactDaoImpl();
+        String name = view.getStringFromUserInput(view.artifactNameQuestion);
+        ArtifactModel artifact = dao.getArtifact();
+        String choice = view.getStringFromUserInput(view.artifactEditChoice);
+        if (choice == "1"){
+          String name = view.getStringFromUserInput(view.artifactNameQuestion);
+          artifact.setName(name);
+        }
+        else if(choice == "2"){
+          String description = view.getStringFromUserInput(view.artifactDescriptionQuestion);
+          artifact.setDescription(description);
+        }
+        else if(choice == "3"){
+          String priceStr = view.getStringFromUserInput(view.artifactPriceQuestion);
+          float price = Float.parseFloat(priceStr);
+          artifact.setPrice(price);
+        }
+        else{
+          System.out.println(view.noSuchOption)
+        }
     }
 
     public void createArtifactCategory(){
--- ArtifactStoreView.java
+++ ArtifactStoreView.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
 class ArtifactStoreView extends AbstractConsoleView{
-    public String artifactNameQuestion = "What should be the artefacts name?";
+    public String artifactNameQuestion = "What is the artefacts name?";
     public String artifactDescriptionQuestion = "Provide artifact description: ";
     public String artifactPriceQuestion = "What should be its price?";
     public String artifactPurcahseQuestion;
     public String artifactCategoryQuestion = "Which category should artifact belong to?";
     public String artifactUnavaliable = "This artifact is unavaliable";
     public String insufficientFunds = "Insufficient funds";
+    public String artifactEditChoice = "Which attribute do you want to edit: \n 1. Name \n 2. Description \n 3. Price";
+    public String noSuchOption = "No such option";
 }
