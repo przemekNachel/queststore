63f0e8279489aed9386e587a378bb48e42c61591
creat73 <nikill1125@gmail.com>
2018-01-11 10:34:58 +0100

    Add ArtifactStoreView blueprint

---
+++ ArtifactStoreView.java
@@ -0,0 +1,19 @@
+class ArtifactStoreView{
+    public String artifactNameQuestion{
+    }
+
+    public String artifactPriceQuestion{
+    }
+
+    public String artifactPurchaseQuestion{
+    }
+
+    public String artifactCategoryQuestion{
+    }
+
+    public String artifacUnavaliable{
+    }
+
+    public String insufficientFunds{
+    }
+}
