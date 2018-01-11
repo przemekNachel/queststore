0fc1ad0a37e5abc0acdbb38de3db9aae75be55f1
Irek Zagan <Sefrys@gmail.com>
2018-01-11 21:21:17 +0100

    Add empty methods

---
+++ QuestDaoImpl.java
@@ -0,0 +1,24 @@
+import java.util.Iterator;
+
+public class QuestDaoImpl implements QuestDao {
+  private static Group<Group<QuestModel>> quests;
+
+  public Group<Group<Quest>> getAllQuests() {
+
+  }
+
+  public QuestModel getQuest(String name) {
+
+  }
+
+  public void addQuest(QuestModel quest) {
+
+  }
+  public void updateQuest(QuestModel quest) {
+
+  }
+
+  public void deleteQuest(QuestModel quest) {
+    
+  }
+}
