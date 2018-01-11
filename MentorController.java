e85022d2f6ca6a22f1e8ee5e23237428d7987f47
Irek Zagan <Sefrys@gmail.com>
2018-01-11 12:49:19 +0100

    Expand createCodecooler method implementation

--- MentorController.java
+++ MentorController.java
@@ -1,8 +1,27 @@
 public class MentorController {
-    public MentorView view;
+    public MentorView view = new MentorView();
 
     public void createCodecooler() {
-      CodecoolerModel codecooler = new CodecoolerModel();
+
+      String nickname = view.getStringFromUserInput("provide user nickname");
+      String email = view.getStringFromUserInput("provide user mail");
+      String password = view.getStringFromUserInput("provide user password");
+
+      CodecoolerModel codecooler = new CodecoolerModel("codecooler", nickname, email, password);
+      UserDaoImpl userDao = new UserDaoImpl();
+
+      // If user getter doesn't find given user, it returns null
+      if (userDao.getUser(codecooler) == null) {
+        userDao.addUser(codecooler);
+      }
+
+      else {
+        view.printLine("User already in database.")
+      }
+
+
+      // instantiate userDaoImpl object
+      // implement adding codecooler
     }
     // Using userDaoImpl, call addUser() method with has the user passed
     // check through day if such user already exists
