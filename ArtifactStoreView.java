30bd3d8c1714da82deaf5fbfb4aba355af46f461
creat73 <nikill1125@gmail.com>
2018-01-11 12:51:36 +0100

    Rebuild ArtifactStoreView class

--- ArtifactStoreView.java
+++ ArtifactStoreView.java
@@ -1,33 +1,10 @@
 import java.util.Scanner;
 
 class ArtifactStoreView{
-    public static String artifactNameQuestion{
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Name your artifact: ");
-      return sc.next();
-    }
-
-    public static String artifactPriceQuestion{
-      Scanner sc = new Scanner(System.in);
-      System.out.println("How much should an artifact cost: ")
-      return sc.nextFloat()
-    }
-
-    public static String artifactDescriptionQuestion{
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Add description: ")
-      return sc.next();
-    }
-
-    public String artifactPurchaseQuestion{
-    }
-
-    public String artifactCategoryQuestion{
-    }
-
-    public String artifacUnavaliable{
-    }
-
-    public String insufficientFunds{
-    }
+    public String artifactNameQuestion = "What should be the artefacts name?"
+    public String artifactPriceQuestion = "What should be its price?"
+    public String artifactPurcahseQuestion;
+    public String artifactCategoryQuestion = "Which category should artifact belong to?;"
+    public String artifactUnavaliable = "This artifact is unavaliable"
+    public String insufficientFunds = "Insufficient funds"
 }
