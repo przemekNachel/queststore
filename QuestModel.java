3d685076cc4e6b9e8dffb373abbbb10de7d32257
Irek Zagan <Sefrys@gmail.com>
2018-01-11 21:52:30 +0100

    Add Quest constructor

--- QuestModel.java
+++ QuestModel.java
@@ -2,4 +2,10 @@ public class QuestModel {
   private String questName;
   private String questDescription;
   private Float questReward;
+
+  public QuestModel(String name, String desc, Float reward) {
+    this.questName = name;
+    this.questDescription = desc;
+    this.questReward = reward;
+  }
 }
