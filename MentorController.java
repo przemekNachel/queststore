2b3472b617af66f4941dbc07137e08cd8f5a9318
Irek Zagan <Sefrys@gmail.com>
2018-01-11 13:29:06 +0100

    Instantiate walletservice object

--- MentorController.java
+++ MentorController.java
@@ -2,13 +2,15 @@ public class MentorController {
     public MentorView view = new MentorView();
 
     public void createCodecooler() {
+      UserDaoImpl userDao = new UserDaoImpl();
+      WalletService wallet = new WalletService();
+
 
       String nickname = view.getStringFromUserInput(view.userNicknameQUestion);
       String email = view.getStringFromUserInput(view.userEmailQuestion);
       String password = view.getStringFromUserInput(view.userPasswordQuestion);
 
       CodecoolerModel codecooler = new CodecoolerModel("codecooler", nickname, email, password);
-      UserDaoImpl userDao = new UserDaoImpl();
 
       // If user getter doesn't find given user, it returns null
       if (userDao.getUser(codecooler) == null) {
@@ -20,8 +22,10 @@ public class MentorController {
       }
     }
     public void assignCodecoolerToGroup() {
-      
-    }
 
+      UserDaoImpl userDao = new UserDaoImpl();
+     //zmien associatedGroups w polu usera, wywolujesz update user z tym userem
+      //
+    }
 
 }
