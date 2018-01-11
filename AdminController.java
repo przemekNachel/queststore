12fc60166615332515a121c795b658644ffb6a97
creat73 <nikill1125@gmail.com>
2018-01-11 14:07:42 +0100

    Add AdminController blueprint

---
+++ AdminController.java
@@ -0,0 +1,28 @@
+import MentorModel;
+
+class AdminController{
+  AdminView view = new AdinView();
+
+  public void createMentor(){
+  }
+
+  public void assignMentorToGroup(){
+
+  }
+
+  public void createGroup(){
+
+  }
+
+  public void editMentor(){
+
+  }
+
+  public void getMentorDisplay(){
+
+  }
+
+  public void createLevel(){
+
+  }
+}
