7c03adf250e91cb0077e6c0edc95d38aafe44578
DrMayx <drmayx>
2018-01-23 21:02:22 +0100

    UserDaoImpl : convert getAllUsers to use SQL queries; make all other methods placeholders for sake of compilation

--- UserDaoImpl.java
+++ UserDaoImpl.java
@@ -1,20 +1,214 @@
 import java.util.Iterator;
+import java.sql.*;
 
 public class UserDaoImpl implements UserDao{
-    private static Group<Group<User>> users;
 
-    public Group<Group<User>> getAllUsers(){
-        return users;
+    private Connection connect = null;
+    private Statement statement = null;
+    private static String JDBC = "jdbc:sqlite:database.db";
+
+    public UserDaoImpl(){
+        try{
+            establishConnection();
+        }catch(Exception e){
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(1);
+        }
+        System.out.println("Connected");
     }
 
+    public Group<Group<User>> getAllUsers(){
+
+        Group<Group<User>> allUsers = new Group<Group<User>>("all users");
+        String query = "SELECT * FROM users";
+
+        ResultSet results = statement.executeQuery(query);
+
+        Group<Group<User>> userGroups = getAllGroups();
+        Group<User> students = getGroup("students", userGroups);
+        Group<User> mentors = getGroup("mentors", userGroups);
+        Group<User> admins = getGroup("admins", userGroups);
+
+        // user credentials:
+        String nickname, password, email;
+        Role role;
+
+        WalletService tmpWallet;
+        Group<Group<User>> tmpGroup;
+        User tempUsr;
+        Group<Group<User>> tmpGroups;
+
+        while(results.next()){
+
+            //get user data
+            tmpGroup = new Group<Group<User>>("associated groups");
+            role = convertRole(getRole(results.getInt("user_id")));
+            nickname = results.getString("nickname");
+            password = results.getString("password");
+            email = results.getString("email");
+
+            if(role == Role.ADMIN){
+                tempUsr = new AdminModel(nickname, password, admins);
+                admins.add(tempUsr);
+            }else if(role == Role.MENTOR){
+                tempUsr = new MentorModel(nickname, email, password, mentors);
+                mentors.add(tempUsr);
+            }else if(role == Role.CODECOOLER){
+                tmpWallet = new WalletService(0);
+                tempUsr = new CodecoolerModel(nickname, email,  password, tmpWallet, students);
+                students.add(tempUsr);
+            }
+            associateGroups(tempUsr, getUserGroup(results.getInt("user_id"), userGroups));
+
+        }
+        return allUsers;
+    }
+
+    private Group<Group<User>> getAllGroups(){
+        Group<Group<User>> associatedGroups;
+        String query = "SELECT group_name " +
+            "FROM user_associations " +
+            "LEFT JOIN group_names  " +
+            "ON user_associations.group_id=group_names.group_id;";
+
+        ResultSet results = statement.executeQuery(query);
+
+        while(results.next()){
+            associatedGroups.add(new Group<User>(results.getString("group_name")));
+        }
+        results.close();
+        return associatedGroups;
+
+    }
+
+    private Group<Group<User>> getUserGroup(int userId, Group<Group<User>> allGroups){
+        Group<Group<User>> associatedGroups;
+        String query = "SELECT group_name " +
+            "FROM group_names " +
+            "LEFT JOIN user_associations  " +
+            "ON group_names.group_id=user_associations.group_id " +
+            "WHERE user_associations.user_id=" + userId +";";
+
+        ResultSet results = statement.executeQuery(query);
+
+        Group<User> currentGroup;
+        Iterator<Group<User>> groupIterator;
+        String resultGroupName;
+        while(results.next()){
+
+            groupIterator = allGroups.getIterator();
+            resultGroupName = results.getString("group_name");
+            while(groupIterator.hasNext()){
+
+                currentGroup = groupIterator.next();
+
+                if(currentGroup.getName().equals(resultGroupName)){
+                    associatedGroups.add(currentGroup);
+                }
+
+            }
+        }
+        results.close();
+        return associatedGroups;
+    }
+
+    private void associateGroups(User user, Group<Group<User>> userGroups){
+        Iterator userGroupsIterator = userGroups.getIterator();
+        while(userGroupsIterator.hasNext()){
+            user.setAssociatedGroups(userGroups);
+        }
+    }
+
+    private Group<User> getGroup(String groupName,
+                                    Group<Group<User>> userGroups){
+
+        Iterator<Group<User>> userGroupIterator = userGroups.getIterator();
+        while(userGroupIterator.hasNext()){
+            Group<User> userGroup = userGroupIterator.next();
+
+            if(userGroup.getName().equals(groupName)){
+                return userGroup;
+            }
+        }
+        return null;
+    }
+
+    private Role convertRole(String roleName){
+        switch(roleName){
+            case "codecooler":
+                return Role.CODECOOLER;
+            case "mentor":
+                return Role.MENTOR;
+            case "admin":
+                return Role.ADMIN;
+        }
+
+        return null;
+
+    }
+
+    private String getRole(int userId){
+
+        String query = "SELECT privilege_name " +
+            "FROM user_privilege_levels " +
+            "LEFT JOIN user_roles  " +
+            "ON user_privilege_levels.privilege_id" +
+            "=user_roles.user_privilege_level " +
+            "WHERE user_roles.user_id = " + userId + ";";
+
+        ResultSet results = statement.executeQuery(query);
+
+        while(results.next()){
+            String privilege = results.getString("privilege_name");
+            results.close();
+            return privilege;
+        }
+        results.close();
+        return null;
+
+    }
+
+
+
+    // placeholders for the sake of compilation
+    public User getUser(String nickname){
+        return new Admin("a","a", new Group<User>("a"));
+    }
+
+    public void addUser(User user){}
+
+    public void updateUser(User user){}
+
+    public boolean deleteUser(User user){return true;}
+
+    public Group<String> getUserGroupNames(){
+        return new Group<String>("a");
+    }
+
+    public Group<User> getUserGroup(String groupName){
+        return new Group<User>("a");
+    }
+
+    public boolean addUserAdherence(User user, String groupName){return true;}
+
+    public void addUserGroup(Group<User> group){}
+
+    public void tmpSetUsers(Group<Group<User>> users){}
+
+
+
+
+    /*
     public User getUser(String nickname){
 
         Iterator<Group<User>> userGroupIterator = UserDaoImpl.users.getIterator();
         while(userGroupIterator.hasNext()){
+
             Group<User> userGroup = userGroupIterator.next();
             Iterator<User> usersIterator = userGroup.getIterator();
 
             while(usersIterator.hasNext()){
+
                 User currentUser = usersIterator.next();
                 if(currentUser.getName().equals(nickname)){
                     return currentUser;
@@ -92,6 +286,7 @@ public class UserDaoImpl implements UserDao{
         }
         return false;
     }
+
     public void addUserGroup(Group<User> group){
         users.add(group);
     }
@@ -124,7 +319,28 @@ public class UserDaoImpl implements UserDao{
         return false;
     }
 
-    public void size(){
+    */
+
+    /*
+     public void size(){
         System.out.println("Size of 'users': " + this.users.size());
+    }
+    */
+
+    private void establishConnection() throws SQLException, ClassNotFoundException{
+
+        Class.forName("org.sqlite.JDBC");
+        this.connect = DriverManager.getConnection(UserDaoImpl.JDBC);
+        connect.setAutoCommit(false);
+        this.statement = connect.createStatement();
+    }
+
+    public void close() throws SQLException{
+        if(statement != null){
+            statement.close();
+        }
+        if(connect != null){
+            connect.close();
+        }
     }
 }
