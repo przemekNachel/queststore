2e15d2e0cc189a2bd839ba5c278c501d8a10f666
creat73 <nikill1125@gmail.com>
2018-01-24 18:15:20 +0100

    LevelDaoImpl: Add getLevelCollection method

--- LevelDaoImpl.java
+++ LevelDaoImpl.java
@@ -2,7 +2,26 @@ import java.util.HashMap;
 import java.sql.*;
 
 public class LevelDaoImpl {
-    public HashMap<Integer, String> getLevelCollection() {
+    public HashMap<Integer, String> getLevelCollection(){
+        try {
+            HashMap<Integer, String> levels = new HashMap<>();
+            Connection databaseConnection = Connect();
+            Statement statement = databaseConnection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM predefined_levels;");
+
+            while (rs.next()) {
+                Integer levelThreshold = rs.getInt("threshold");
+                String levelName = rs.getString("level_name");
+                levels.put(levelThreshold, levelName);
+            }
+
+            return levels;
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+            return null;
+        }
     }
 
     public boolean saveLevelCollection(HashMap<Integer, String> levels,){
