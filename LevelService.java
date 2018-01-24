2728a7cd1584ad84fe52133701986594b8bd1e23
creat73 <nikill1125@gmail.com>
2018-01-24 18:49:07 +0100

    LevelService: Add initializeLevels method

---
+++ LevelService.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class LevelService {
+    public void initializeLevels(){
+        LevelDaoImpl dao = new LevelDaoImpl();
+        HashMap<Integer, String> levels = dao.getLevelCollection();
+        Level.setLevels(levels);
+    }
+
+    public void saveLevels(HashMap<Integer, String> levels){
+    }
+}
