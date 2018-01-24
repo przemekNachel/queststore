3cc6f7d4230851cdef2fedb6eee5d5e239a22b9d
Irek Zagan <Sefrys@gmail.com>
2018-01-24 15:39:06 +0100

    Add get all quests from SQL DB

--- QuestDaoImpl.java
+++ QuestDaoImpl.java
@@ -1,6 +1,8 @@
 import java.sql.*;
+import java.util.Iterator;
 import java.util.Objects;
 
+@SuppressWarnings("ALL")
 public class QuestDaoImpl implements QuestDao {
     @Override
     public Connection connectToDatabase() throws SQLException {
@@ -85,9 +87,67 @@ public class QuestDaoImpl implements QuestDao {
         return new QuestModel(questName, questDescr, questReward);
     }
 
+
+
     @Override
     public Group<Group<QuestModel>> getAllQuests() throws SQLException {
+        Group<Group<QuestModel>> allQuests = new Group<>("All quests");
+        Group<String> groupNames = getQuestGroupNames();
+        Iterator<String> groupNamesIter = groupNames.getIterator();
 
-        return null;
+        while(groupNamesIter.hasNext()) {
+            allQuests.add(getQuestGroup(groupNamesIter.next()));
+        }
+
+        return allQuests;
+    }
+
+    @Override
+    public Group<String> getQuestGroupNames() throws SQLException {
+        Group<String> groupsNames = new Group<>("Group name");
+
+        Connection con = connectToDatabase();
+        Statement stmt = Objects.requireNonNull(con).createStatement();
+
+        String sql = "SELECT group_name FROM group_names";
+        ResultSet rs = stmt.executeQuery(sql);
+
+        while(rs.next()) {
+            String group_name = rs.getString("group_name");
+            groupsNames.add(group_name);
+        }
+        return groupsNames;
+    }
+
+    @Override
+    public Group<QuestModel> getQuestGroup(String groupName) throws SQLException {
+        Group<QuestModel> group = new Group<>(groupName);
+
+        Connection con = connectToDatabase();
+        Statement stmt = Objects.requireNonNull(con).createStatement();
+
+        String sql = "SELECT\n" +
+                "  quest_store.quest_id, name, descr, reward, group_names.group_name\n" +
+                "FROM\n" +
+                "  quest_store\n" +
+                "  INNER JOIN quest_associations ON quest_associations.association_id = quest_store.quest_id\n" +
+                "  INNER JOIN group_names ON group_names.group_id = quest_associations.group_id\n" +
+                " WHERE group_name='" + groupName + "';";
+
+        ResultSet rs = stmt.executeQuery(sql);
+
+        while(rs.next()) {
+            String questName = rs.getString("name");
+            String questDescr = rs.getString("descr");
+            Integer questReward = rs.getInt("reward");
+            group.add(new QuestModel(questName, questDescr, questReward));
+        }
+
+        stmt.close();
+        rs.close();
+        con.close();
+
+        System.out.println(group);
+        return group;
     }
 }
