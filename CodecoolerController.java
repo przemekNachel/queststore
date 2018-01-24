45c2d0b654b5c586d70f1b3c38a918ea1611da75
Michal <michal.hanebach@gmail.com>
2018-01-24 14:27:49 +0100

    CodecoolerController: enable getting an artifact for usage

--- CodecoolerController.java
+++ CodecoolerController.java
@@ -57,7 +57,7 @@ public class CodecoolerController {
 
   public void useArtifact() {
     String artifactName = view.getStringFromUserInput(view.artifactNameQuestion);
-    //currentUser.getArtifact(artifactName); // comented for the sake of smooth work
+    currentUser.getArtifact(artifactName);
     view.printLine("Artifact has been used!");
   }
 
