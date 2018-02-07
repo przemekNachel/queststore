4081dd4da1511c48fe43c39795d2f994eb8d8756
Michal Hanebach <michal.hanebach@gmail.com>
2018-02-07 17:37:24 +0100

    ArtifactStoreController: itegrate with UserService -> buying as a group finally works

--- src/artifact/ArtifactStoreController.java
+++ src/artifact/ArtifactStoreController.java
@@ -4,6 +4,7 @@ import console.menu.Menu;
 import console.menu.MenuOption;
 import generic_group.Group;
 import user.codecooler.CodecoolerModel;
+import user.service.UserService;
 import user.user.User;
 import user.user.UserDaoImpl;
 import user.wallet.WalletService;
@@ -118,17 +119,10 @@ public class ArtifactStoreController{
 
     private Group<CodecoolerModel> getConsumerGroup(CodecoolerModel codecooler) {
 
-        UserDaoImpl userDao = new UserDaoImpl();
+        UserService userSvc = new UserService();
 
         // get all user groups to choose from and display them
-        Group<String> allowedGroupNames = null;
-
-        try{
-            allowedGroupNames = userDao.getUserGroupNames();
-        } catch (SQLException e) {
-            view.printSQLException(e);
-            return null;
-        }
+        Group<String> allowedGroupNames = userSvc.getUserGroupNames();
 
         // get group which will crowd-fund the artifact
         Group<CodecoolerModel> codecoolers = null;
@@ -137,19 +131,16 @@ public class ArtifactStoreController{
         do {
             String consumerGroupName = view.getStringFromUserInput(view.chooseGroup);
             if (allowedGroupNames.contains(consumerGroupName)) {
-                try{
-                    Group<User> users = userDao.getUserGroup(consumerGroupName);
-                    // buy the artifact as a group
-                    codecoolers = new Group<>("Codecooler(s) buying an artifact");
-                    for (User currentUser : users) {
 
-                        codecoolers.add((CodecoolerModel)currentUser);
-                    }
-                } catch (SQLException e) {
-                    view.printSQLException(e);
-                    return null;
+                Group<User> users = userSvc.getUserGroup(consumerGroupName);
+
+                codecoolers = new Group<>("Codecooler(s) buying an artifact");
+                for (User currentUser : users) {
+
+                    codecoolers.add((CodecoolerModel)currentUser);
                 }
                 providedExistentGroupName = true;
+
             } else {
                 if (consumerGroupName.equals("ALONE")) {
 
@@ -157,7 +148,6 @@ public class ArtifactStoreController{
                     codecoolers.add(codecooler);
                     wantToBuyAlone = true;
                 } else if (consumerGroupName.equals(view.magicExitString)) {
-
                     return null;
                 } else {
 
@@ -170,8 +160,6 @@ public class ArtifactStoreController{
     }
 
     public void buyProductProcess(CodecoolerModel codecooler){
-
-        UserDaoImpl userDao = new UserDaoImpl();
 
         view.printLine(view.abortHint);
         String artifactName = getArtifactNameFromUserInput();
@@ -187,20 +175,17 @@ public class ArtifactStoreController{
         }
 
         ArtifactModel boughtArtifact = buyArtifact(artifactName, consumers);
+
         if (boughtArtifact == null) {
 
             return;
         }
         /* purchase process succeeded */
         codecooler.addArtifact(boughtArtifact);
-        try {
+        UserService userSvc = new UserService();
+        for (User user : consumers) {
 
-            for (User user : consumers) {
-
-                userDao.updateUser(user);
-            }
-        } catch (SQLException e) {
-            view.printSQLException(e);
+            userSvc.updateUser(user);
         }
     }
 
