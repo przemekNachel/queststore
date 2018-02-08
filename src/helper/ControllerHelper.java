87b105d66c38b10b58408c7218ae5d45539710ae
Michal Hanebach <michal.hanebach@gmail.com>
2018-02-08 10:03:34 +0100

    ControllerHelper: add helper class for shared controller functionality

---
+++ src/helper/ControllerHelper.java
@@ -0,0 +1,66 @@
+package helper;
+
+import console.menu.AbstractConsoleView;
+import generic_group.Group;
+import user.user.User;
+
+public class ControllerHelper {
+
+    private AbstractConsoleView view;
+
+    public ControllerHelper(AbstractConsoleView view) {
+
+        this.view = view;
+    }
+
+    public Group<String> userGroupToStringGroup(Group<User> userGroup) {
+
+        Group<String> stringGroup = new Group<>("user names");
+        for (User user : userGroup) {
+
+            stringGroup.add(user.getName());
+        }
+        return stringGroup;
+    }
+
+    public String getNameFromUserInput(String prompt, String disallowedMessage, Group<String> allowedNames) {
+
+        String name;
+        boolean providedValidName = false;
+        do {
+
+            name = view.getStringFromUserInput(prompt);
+            if (allowedNames.contains(name)) {
+
+                providedValidName = true;
+            } else {
+
+                view.printLine(disallowedMessage);
+            }
+
+        } while(!providedValidName);
+        return name;
+    }
+
+
+    /* Returns the first input string that does not occur in disallowedNames */
+    public String getExclusiveNameFromUserInput(String prompt, String disallowedMessage, Group<String> disallowedNames) {
+
+        String name;
+        boolean providedValidName = false;
+        do {
+
+            name = view.getStringFromUserInput(prompt);
+            if (!disallowedNames.contains(name)) {
+
+                providedValidName = true;
+            } else {
+
+                view.printLine(disallowedMessage);
+            }
+
+        } while(!providedValidName);
+        return name;
+    }
+
+}
