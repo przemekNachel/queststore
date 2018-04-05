6ff237cd22bb2aa40f51359351133348dbaf2070
siemaeniu500 <siemaeniu500@gmail.com>
2018-04-05 11:04:13 +0200

    Add switching content by clicking on left bar options.

--- web/resources/js/app.js
+++ web/resources/js/app.js
@@ -1,14 +1,41 @@
 let dropdown_toggle = document.querySelector('.dropdown');
+let switch_prefix = 'switch-';
+let last_active_content = document.getElementById('dashboard');
+let selected_option = document.getElementById('switch-dashboard');
+
+function switchContent(classID) {
+    last_active_content.classList.add('hidden');
+
+    last_active_content = document.getElementById(classID.replace(switch_prefix, ''));
+
+    last_active_content.classList.remove('hidden');
+
+    selected_option.classList.add('selected');
+
+    document.querySelector('.content-header-label').textContent = selected_option.childNodes[3].textContent;
+
+
+}
 
 document.addEventListener('click', function (event) {
     let className = event.target.className;
+    let classID = event.target.id;
+    let parentClassID = event.target.parentNode.id;
+    if (classID.includes(switch_prefix)) {
+        selected_option.classList.remove('selected');
+        selected_option = event.target;
+        switchContent(classID);
+    }
+    if(parentClassID.includes(switch_prefix)) {
+        selected_option.classList.remove('selected');
+        selected_option = event.target.parentNode;
+        switchContent(parentClassID);
+    }
 
     if (className === "welcome-box") {
         dropdown_toggle.classList.toggle('hidden');
-        console.log('toggle');
     } else {
         dropdown_toggle.classList.add('hidden');
-        console.log('remove')
     }
 
 });
