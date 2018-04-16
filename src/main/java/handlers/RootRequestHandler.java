cbb1e74f925b48ab2464406d94c6e91732864340
siemaeniu500 <siemaeniu500@gmail.com>
2018-04-16 23:59:35 +0200

    Add basic implementation of RootRequestHandler.

--- src/main/java/handlers/RootRequestHandler.java
+++ src/main/java/handlers/RootRequestHandler.java
@@ -2,13 +2,31 @@ package handlers;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import user.user.User;
+import utils.RequestRedirector;
+import utils.SessionManager;
 
 import java.io.IOException;
 
 public class RootRequestHandler implements HttpHandler {
 
+    private final SessionManager sessionManager;
+    private final RequestRedirector redirector;
+
+    public RootRequestHandler(SessionManager sessionManager, RequestRedirector redirector) {
+        this.sessionManager = sessionManager;
+        this.redirector = redirector;
+    }
+
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+        User user = sessionManager.getUserFromSession(httpExchange);
+        if (user == null) {
+            redirector.redirect(httpExchange, "/");
+        } else {
+            sessionManager.createSessionAndAssignToCookie(httpExchange, user);
+            redirector.redirect(httpExchange, user);
+        }
 
     }
 }
