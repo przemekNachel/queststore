5a0b9afd4caf3e2346b87fcc06aa2dddf25b71ca
siemaeniu500 <siemaeniu500@gmail.com>
2018-04-16 15:09:26 +0200

    Add starting HTTPServer on startup.

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,10 +1,20 @@
 package app;
 
-import login.LoginController;
+import com.sun.net.httpserver.HttpServer;
+import handlers.RootRequestHandler;
+import handlers.StaticResourcesHandler;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
 public class App {
 
-    public static void main(String[] args) {
-        new LoginController().start();
+    public static void main(String[] args) throws IOException {
+//        new LoginController().start();
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        httpServer.createContext("/", new RootRequestHandler());
+        httpServer.createContext("/static", new StaticResourcesHandler());
+        httpServer.start();
     }
 }
