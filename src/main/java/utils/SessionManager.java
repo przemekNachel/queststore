312500c24b8f4de0a2259020deeb34874c9ef1c1
siemaeniu500 <siemaeniu500@gmail.com>
2018-04-17 00:01:53 +0200

    Add basic implementation of SessionManager class.

---
+++ src/main/java/utils/SessionManager.java
@@ -0,0 +1,45 @@
+package utils;
+
+import com.sun.net.httpserver.HttpExchange;
+import user.user.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SessionManager {
+
+    private List<Session> sessions = new ArrayList<>();
+
+    public SessionManager() {
+
+    }
+
+    public User getUserFromSession(HttpExchange exchange) {
+        User user = null;
+        String cookiesHeader = exchange.getRequestHeaders().getFirst("Cookie");
+        String sessionID = CookiesUtil.getSessionID(cookiesHeader);
+        System.out.println(sessionID);
+        if (sessionID == null) {
+            return user;
+        }
+
+        for (Session session : sessions) {
+            if (session.getId().equalsIgnoreCase(sessionID)) {
+                return session.getUser();
+            }
+        }
+        return user;
+    }
+
+    public void createSessionAndAssignToCookie(HttpExchange exchange, User user) {
+        Session session = new Session(user);
+        CookiesUtil.addSessionCookie(exchange, session);
+        sessions.add(session);
+    }
+
+    public void expireUserSession(HttpExchange exchange) {
+
+    }
+
+
+}
