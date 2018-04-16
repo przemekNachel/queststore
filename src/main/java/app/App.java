2780fa5e53f8d1850a72ee82ee5f934f800b10f2
siemaeniu500 <siemaeniu500@gmail.com>
2018-04-16 23:58:16 +0200

    Improve starting class.

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,8 +1,12 @@
 package app;
 
 import com.sun.net.httpserver.HttpServer;
+import handlers.LoginRequestHandler;
+import handlers.LogoutRequestHandler;
 import handlers.RootRequestHandler;
 import handlers.StaticResourcesHandler;
+import utils.RequestRedirector;
+import utils.SessionManager;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -11,10 +15,15 @@ public class App {
 
     public static void main(String[] args) throws IOException {
 //        new LoginController().start();
+        SessionManager sessionManager = new SessionManager();
+
+        RequestRedirector redirector = new RequestRedirector(sessionManager);
         HttpServer httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
 
-        httpServer.createContext("/", new RootRequestHandler());
+        httpServer.createContext("/", new RootRequestHandler(sessionManager, redirector));
         httpServer.createContext("/static", new StaticResourcesHandler());
+        httpServer.createContext("/login", new LoginRequestHandler(sessionManager, redirector));
+        httpServer.createContext("/logout", new LogoutRequestHandler(sessionManager, redirector));
         httpServer.start();
     }
 }
