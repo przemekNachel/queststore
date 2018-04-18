9f1a7470d99e790b0790f7e3e4b60f1fe4a79c73
Jacek <jacek.ragann@gmail.com>
2018-04-18 15:19:44 +0200

    Create working artifact request handler for "POST" method and add created user to the database

--- src/main/java/handlers/ArtifactRequestHandler.java
+++ src/main/java/handlers/ArtifactRequestHandler.java
@@ -1,11 +1,20 @@
 package handlers;
 
+import artifact.ArtifactDao;
+import artifact.ArtifactDaoImpl;
+import artifact.ArtifactModel;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import user.user.User;
+import utils.ParametersUtil;
 import utils.RequestRedirector;
 import utils.SessionManager;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.Map;
 
 public class ArtifactRequestHandler implements HttpHandler {
 
@@ -20,6 +29,46 @@ public class ArtifactRequestHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+        User user = sessionManager.getUserFromSession(httpExchange);
+        if (httpExchange.getRequestMethod().equals("POST")) {
+            String unparsedPostData = new BufferedReader(new InputStreamReader(httpExchange.getRequestBody())).readLine();
 
+            Map<String, String> parameters = ParametersUtil.parseParameters(unparsedPostData);
+            System.out.println(unparsedPostData);
+
+            ArtifactModel artifact = createArtifact(parameters);
+            addArtifactToDatabase(artifact, parameters.get("type"));
+        }
+        redirector.redirect(httpExchange, user);
+    }
+
+    private ArtifactModel createArtifact(Map<String, String> parameters) {
+        String name = parameters.get("name");
+        String price = parameters.get("price");
+        String description = parameters.get("description");
+        return new ArtifactModel(name, description, Integer.parseInt(price));
+    }
+
+    private void addArtifactToDatabase(ArtifactModel artifact, String type) {
+        ArtifactDao dao = new ArtifactDaoImpl();
+        type = convertType(type);
+        try {
+            dao.addArtifact(artifact);
+            dao.addArtifactAdherence(artifact.getName(), type);
+            System.out.println("Success");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String convertType(String type) {
+        switch (type) {
+            case "normal":
+                return "artifact_basic";
+            case "magic":
+                return "artifact_magic";
+            default:
+                return "unknown";
+        }
     }
 }
