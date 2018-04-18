a5d1ec0d63e6174fabe20a7715cda8780f4dd86a
przemekNachel <przemekNachel@gmail.com>
2018-04-18 10:16:57 +0200

    Add buy artifact menu

--- src/main/java/template/CodecoolerTemplateResolver.java
+++ src/main/java/template/CodecoolerTemplateResolver.java
@@ -1,29 +1,40 @@
 package template;
 
+import artifact.ArtifactDaoImpl;
 import level.LevelService;
 import user.codecooler.CodecoolerModel;
 import user.user.User;
 
+import java.sql.SQLException;
+
 public class CodecoolerTemplateResolver {
 
     private ViewData template;
+    private ArtifactDaoImpl artifactDao;
     private CodecoolerModel codecooler;
 
     public CodecoolerTemplateResolver(ViewData template, User codecooler) {
         this.template = template;
         this.codecooler = (CodecoolerModel) codecooler;
+        artifactDao = new ArtifactDaoImpl();
     }
 
     public ViewData getTemplate() {
-        initializeVariables();
+        try {
+            initializeVariables();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return template;
     }
 
-    private void initializeVariables() {
+    private void initializeVariables() throws SQLException {
         new LevelService().initializeLevels();
         template.setVariable("user", codecooler);
         template.setVariable("level", codecooler.getLevel().getCurrentLevel());
         template.setVariable("money", codecooler.getWallet().getBalance());
         template.setVariable("classes", codecooler.getCodecoolerGroupDisplay());
+        template.setVariable("basic_artifacts", artifactDao.getArtifactGroup("artifact_basic"));
+        template.setVariable("magic_artifacts", artifactDao.getArtifactGroup("artifact_magic"));
     }
 }
