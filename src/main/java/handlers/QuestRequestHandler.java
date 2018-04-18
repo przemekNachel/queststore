e3c5abe0b9de13d4d940255e02716369fb2b52eb
adamlup <adamlupi@gmail.com>
2018-04-18 21:23:23 +0200

    Add create new quest request handler

--- src/main/java/handlers/QuestRequestHandler.java
+++ src/main/java/handlers/QuestRequestHandler.java
@@ -2,10 +2,20 @@ package handlers;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import quest.QuestDao;
+import quest.QuestDaoImpl;
+import quest.QuestModel;
+import user.user.Role;
+import user.user.User;
+import utils.ParametersUtil;
 import utils.RequestRedirector;
 import utils.SessionManager;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+import java.util.Map;
 
 public class QuestRequestHandler implements HttpHandler {
 
@@ -20,6 +30,29 @@ public class QuestRequestHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+        User user = sessionManager.getUserFromSession(httpExchange);
+        if (user.getRole() == Role.MENTOR) {
+            if (httpExchange.getRequestMethod().equalsIgnoreCase("post")) {
+                String postInputData = new BufferedReader(new InputStreamReader(httpExchange.getRequestBody())).readLine();
 
+                Map<String, String> parameters = ParametersUtil.parseParameters(postInputData);
+                addNewQuest(parameters);
+            }
+        }
+        redirector.redirect(httpExchange, user);
+    }
+
+    public void addNewQuest(Map<String, String> parameters) {
+        QuestDaoImpl questDao = new QuestDaoImpl();
+        String name = parameters.get("name");
+        String stringReward = parameters.get("reward");
+        String description = parameters.get("description");
+        int reward = Integer.parseInt(stringReward);
+        QuestModel newQuest = new QuestModel(name, description, reward);
+        try {
+            questDao.addQuest(newQuest);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
