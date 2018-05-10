9d8c3fa0a23ce3b3b6fafb48fac07db51d8d145e
adamlup <adamlupi@gmail.com>
2018-05-10 09:43:07 +0200

    delete not needed methods

--- src/main/java/quest/QuestDao.java
+++ src/main/java/quest/QuestDao.java
@@ -8,10 +8,6 @@ import java.sql.SQLException;
 public interface QuestDao {
     Connection connectToDatabase() throws SQLException;
 
-    Group<Group<QuestModel>> getAllQuests() throws SQLException;
-
-    Group<String> getQuestGroupNames() throws SQLException;
-
     Group<QuestModel> getQuestGroup(String groupName) throws SQLException;
 
     QuestModel getQuest(String name) throws SQLException;
